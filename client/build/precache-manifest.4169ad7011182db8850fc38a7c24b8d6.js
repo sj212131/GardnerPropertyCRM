5039a32bd3a4c301466c5e3cd2c53da58fa398f3
sj212131 <sj212131@hotmail.com>
2022-12-05 10:47:29 -0500

    update deploy file

---
+++ client/build/precache-manifest.4169ad7011182db8850fc38a7c24b8d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6e08f083b404618501",
+    "url": "/static/css/main.33968172.chunk.css"
+  },
+  {
+    "revision": "0a6e08f083b404618501",
+    "url": "/static/js/main.9f422962.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f18863c4be41a54ef44a",
+    "url": "/static/js/2.fa7bd0a6.chunk.js"
+  },
+  {
+    "revision": "f24e9a9c8748db120202d458c60dd21a",
+    "url": "/static/media/homeicon.f24e9a9c.jpg"
+  },
+  {
+    "revision": "34c1482ab9e9061291c9c908b30e96c2",
+    "url": "/static/media/Nav-background.34c1482a.png"
+  },
+  {
+    "revision": "466fe6431b4645d31487d53afbf9906d",
+    "url": "/static/media/background.466fe643.png"
+  },
+  {
+    "revision": "ecc61ee9a7214a23255562f6d3203dc6",
+    "url": "/index.html"
+  }
+];
