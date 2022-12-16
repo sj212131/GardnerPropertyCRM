8460e356557841d92a18693c0ce8c8d89a3efbcb
sj212131 <sj212131@hotmail.com>
2022-12-16 14:21:06 -0500

    Updates

---
+++ precache-manifest.455a9a16da2392080e7a7f831fbd6679.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3e8a660c577d73997e9",
+    "url": "/GardnerPropertyCRM/static/css/main.88413ca3.chunk.css"
+  },
+  {
+    "revision": "b3e8a660c577d73997e9",
+    "url": "/GardnerPropertyCRM/static/js/main.9f422962.chunk.js"
+  },
+  {
+    "revision": "2d22cd23003164d3a224",
+    "url": "/GardnerPropertyCRM/static/js/runtime~main.60fec674.js"
+  },
+  {
+    "revision": "f18863c4be41a54ef44a",
+    "url": "/GardnerPropertyCRM/static/js/2.fa7bd0a6.chunk.js"
+  },
+  {
+    "revision": "f24e9a9c8748db120202d458c60dd21a",
+    "url": "/GardnerPropertyCRM/static/media/homeicon.f24e9a9c.jpg"
+  },
+  {
+    "revision": "34c1482ab9e9061291c9c908b30e96c2",
+    "url": "/GardnerPropertyCRM/static/media/Nav-background.34c1482a.png"
+  },
+  {
+    "revision": "466fe6431b4645d31487d53afbf9906d",
+    "url": "/GardnerPropertyCRM/static/media/background.466fe643.png"
+  },
+  {
+    "revision": "3aebfd231bd6700587f23aacfa4559cd",
+    "url": "/GardnerPropertyCRM/index.html"
+  }
+];
